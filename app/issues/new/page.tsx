54a5d190de158084112f44e0d5d23f6f89fa4140
ZFarooq77 <farooqzainab77@gmail.com>
2024-05-28 20:14:51 +0500

    Handle Form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,18 +1,77 @@
 "use client";
 import { Button, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title">
-        {/* <TextField.Input placeholder="Title" /> */}
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
+      <TextField.Root placeholder="Title" {...register("title")}>
+        {/* <TextField.Input placeholder="Title" {...register("title")} /> */}
       </TextField.Root>
-      <SimpleMDE placeholder="Description" />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field, fieldState, formState }) => (
+          <SimpleMDE {...field} placeholder="Description" />
+        )}
+      />
+
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
 export default NewIssuePage;
+
+// "use client";
+// import { Button, TextField } from "@radix-ui/themes";
+// import SimpleMDE from "react-simplemde-editor";
+// import { useForm, Controller } from "react-hook-form";
+// import "easymde/dist/easymde.min.css";
+
+// interface IssueForm {
+//   title: string;
+//   description: string;
+// }
+
+// const NewIssuePage = () => {
+//   const { register, control, handleSubmit } = useForm<IssueForm>();
+
+//   return (
+//     <form
+//       className="max-w-xl space-y-3"
+//       onSubmit={handleSubmit((data) => console.log(data))}
+//     >
+//       <TextField.Root placeholder="Title" {...register("title")}>
+//         {/* <TextField.Input placeholder="Title" /> */}
+//       </TextField.Root>
+//       <Controller
+//         name="description"
+//         control={control}
+//         render={(field) => <SimpleMDE placeholder="Description" {...field} />}
+//       />
+
+//       <Button>Submit New Issue</Button>
+//     </form>
+//   );
+// };
+
+// export default NewIssuePage;
